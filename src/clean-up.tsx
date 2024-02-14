c3bfe7c106c4a3701f430c7242f6c5047c7ac30c
Ashok <ashokbaruaakas@gmail.com>
2024-02-14 14:28:26 +0600

    Pull contributions

--- src/clean-up.tsx
+++ src/clean-up.tsx
@@ -1,17 +1,18 @@
 import { showToast, Toast } from "@raycast/api";
 import { brewCleanup } from "./brew";
 import { showActionToast, showFailureToast, wait } from "./utils";
+import { preferences } from "./preferences";
 
 export default async (): Promise<void> => {
   try {
-    const abort = showActionToast({ title: "Cleaning up everything at once" + String.ellipsis, cancelable: true });
-    await brewCleanup(abort);
+    const abort = showActionToast({
+      title: "Cleaning files & packages from the cache" + String.ellipsis,
+      cancelable: true,
+    });
+    await brewCleanup(preferences.withoutThreshold, abort);
     showToast(Toast.Style.Success, "Cleaning completed");
   } catch (err) {
     await showFailureToast("Cleaning failed", err as Error);
-    // Wait around until user has had chance to click the Toast action.
-    // Note this only works for "no view" commands (actions still break when popping a view based command).
-    // See: https://raycastapp.slack.com/archives/C01E6LWGXJ8/p1642676284027700
     await wait(3000);
   }
 };
